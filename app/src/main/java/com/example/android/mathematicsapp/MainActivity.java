dd20bbe36391dd5f62c7bee81637ca5141638ebe
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-11-20 13:58:30 +0100

    TODO for the mathematics app for now.

--- app/src/main/java/com/example/android/mathematicsapp/MainActivity.java
+++ app/src/main/java/com/example/android/mathematicsapp/MainActivity.java
@@ -10,6 +10,15 @@ import com.example.android.mathematicsapp.snakegame.SnakeActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // TODO make main menu better.
+    // TODO add the essential buttons...
+
+    // TODO add how many coins you have in game.
+    // TODO save the coins in game.
+
+    // TODO add new activities for the buttons.
+    // TODO add small hidden button for the snake game.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
