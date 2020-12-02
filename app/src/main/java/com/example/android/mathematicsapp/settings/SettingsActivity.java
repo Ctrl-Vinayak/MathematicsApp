5be0f1c92d2c2d5515dd375e5038d965e9045883
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-12-02 20:02:22 +0100

    homescreen button for settings works fine, for now, homescreen design is done, functionality of the buttons coming soon.

--- app/src/main/java/com/example/android/mathematicsapp/settings/SettingsActivity.java
+++ app/src/main/java/com/example/android/mathematicsapp/settings/SettingsActivity.java
@@ -1,17 +1,27 @@
 package com.example.android.mathematicsapp.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.android.mathematicsapp.R;
+import com.example.android.mathematicsapp.homescreen.HomeScreenActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_settings);
 
+        findViewById(R.id.setting_back_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SettingsActivity.this, HomeScreenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
