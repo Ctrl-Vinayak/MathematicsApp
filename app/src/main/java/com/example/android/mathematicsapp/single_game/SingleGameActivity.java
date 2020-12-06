661732c4dd9aaed4dae0783eb5ce56d60078aeba
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2020-12-06 17:17:43 +0100

    single game apply theme added, arrow up, down and number needs theme too. w.i.p

--- app/src/main/java/com/example/android/mathematicsapp/single_game/SingleGameActivity.java
+++ app/src/main/java/com/example/android/mathematicsapp/single_game/SingleGameActivity.java
@@ -3,8 +3,11 @@ package com.example.android.mathematicsapp.single_game;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.GradientDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -17,6 +20,16 @@ public class SingleGameActivity extends AppCompatActivity {
 
     private RelativeLayout _singleGameActionBar;
     private LinearLayout _singleGameBackground;
+
+    private RelativeLayout _bitterEndBtn;
+    private RelativeLayout _minuteChallengeBtn;
+    private RelativeLayout _speedChallengeBtn;
+    private RelativeLayout _noTimeLimitBtn;
+
+    private ImageView _bitterEndGraph;
+    private ImageView _minuteChallengeGraph;
+    private ImageView _speedChallengeGraph;
+    private ImageView _noTimeLimitGraph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +46,48 @@ public class SingleGameActivity extends AppCompatActivity {
         _singleGameBackground = (LinearLayout) findViewById(R.id.single_game_background);
         _singleGameActionBar.setBackgroundColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
         _singleGameBackground.setBackgroundColor(prefs.getInt("currentBgKey", getApplicationContext().getResources().getColor(R.color.light_bg)));
+
+        // buttons
+        _bitterEndBtn = (RelativeLayout) findViewById(R.id.bitter_end_btn);
+        _bitterEndBtn.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableBitterEndBtn = (GradientDrawable) _bitterEndBtn.getBackground();
+        drawableBitterEndBtn.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        _minuteChallengeBtn = (RelativeLayout) findViewById(R.id.minute_challenge_btn);
+        _minuteChallengeBtn.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableMinuteChallenge = (GradientDrawable) _minuteChallengeBtn.getBackground();
+        drawableMinuteChallenge.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        _speedChallengeBtn = (RelativeLayout) findViewById(R.id.speed_challenge_btn);
+        _speedChallengeBtn.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableSpeedChallenge = (GradientDrawable) _speedChallengeBtn.getBackground();
+        drawableSpeedChallenge.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        _noTimeLimitBtn = (RelativeLayout) findViewById(R.id.no_time_limit_btn);
+        _noTimeLimitBtn.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableNoTimeLimit = (GradientDrawable) _noTimeLimitBtn.getBackground();
+        drawableNoTimeLimit.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        // graphs
+        _bitterEndGraph = (ImageView) findViewById(R.id.graph_bitter_end_btn);
+        _bitterEndGraph.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableBitterEndGraph = (GradientDrawable) _bitterEndGraph.getBackground();
+        drawableBitterEndGraph.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        _minuteChallengeGraph = (ImageView) findViewById(R.id.graph_minute_challenge_btn);
+        _minuteChallengeGraph.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableMinuteChallengeGraph = (GradientDrawable) _minuteChallengeGraph.getBackground();
+        drawableMinuteChallengeGraph.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        _speedChallengeGraph = (ImageView) findViewById(R.id.graph_speed_challenge_btn);
+        _speedChallengeGraph.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableSpeedChallengeGraph = (GradientDrawable) _speedChallengeGraph.getBackground();
+        drawableSpeedChallengeGraph.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
+
+        _noTimeLimitGraph = (ImageView) findViewById(R.id.graph_no_time_limit_btn);
+        _noTimeLimitGraph.setBackgroundResource(R.drawable.customborder1);
+        GradientDrawable drawableNoTimeLimitGraph = (GradientDrawable) _noTimeLimitGraph.getBackground();
+        drawableNoTimeLimitGraph.setColor(prefs.getInt("currentNonBgKey", getApplicationContext().getResources().getColor(R.color.neon_blue)));
     }
 
     private void changeText() {
