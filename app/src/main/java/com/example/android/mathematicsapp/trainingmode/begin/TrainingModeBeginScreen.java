6423bc08391e4d601332f0c83415c97edbb8ccf2
VinvinVinayakvinVin <vinayakshuklakalapnat@live.nl>
2021-02-15 02:29:18 +0100

    removed todo list (:

--- app/src/main/java/com/example/android/mathematicsapp/trainingmode/begin/TrainingModeBeginScreen.java
+++ app/src/main/java/com/example/android/mathematicsapp/trainingmode/begin/TrainingModeBeginScreen.java
@@ -21,9 +21,6 @@ import com.example.android.mathematicsapp.trainingmode.exerciseTheory.TheoryTwo;
 
 public class TrainingModeBeginScreen extends AppCompatActivity {
 
-    // TODO add a few extra exercises in this app!
-    // TODO this is for the presentation, so I can talk more about my app (:
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
